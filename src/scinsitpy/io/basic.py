28176ed0ba4d212ad6ca8177b3ef09e2d1b55fd2
ucagenomix <lebrigand@ipmc.cnrs.fr>
2023-05-03 11:17:50 +0200

    first commit

---
+++ src/scinsitpy/io/basic.py
@@ -0,0 +1,17 @@
+from anndata import AnnData
+
+
+def basic_io(adata: AnnData) -> int:
+    """Generate a basic plot for an AnnData object.
+
+    Parameters
+    ----------
+    adata
+        The AnnData object to preprocess.
+
+    Returns
+    -------
+    Some integer value.
+    """
+    print("Import matplotlib and implement a plotting function here.")
+    return 0
