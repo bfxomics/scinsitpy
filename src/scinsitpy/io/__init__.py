61a92d8e3813a2b0531c8ed0f0f58aee770af3be
ucagenomix <lebrigand@ipmc.cnrs.fr>
2023-05-03 12:01:43 +0200

    go for it

--- src/scinsitpy/io/__init__.py
+++ src/scinsitpy/io/__init__.py
@@ -1,1 +1,8 @@
-from .basic import basic_io
+from .basic import basic_io, filter_and_run_scanpy, load_merscope, save_merscope
+
+__all__ = [
+    "filter_and_run_scanpy",
+    "load_merscope",
+    "save_merscope",
+    "basic_io",
+]
